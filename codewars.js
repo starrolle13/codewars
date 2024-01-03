8e134cf223eff293aa3423bae435ae8fe3b3fb09
Star Rolle <starnesharolle13@gmail.com>
2024-01-03 00:17:41 -0500

    first commit

---
+++ codewars.js
@@ -0,0 +1,12 @@
+// January 2nd 2023
+// I completed a codewar challenge and then optimized it to
+// a ternary and tried to guess the complexity
+// I am going to start asking the chat what the complexity is
+// and then starting to ask why
+
+function boolToWord( bool ){
+    //...
+    let string = "";
+    bool === true? string = "Yes": string = "No"
+    return string;
+  }
