a9529828ae6341c9085416532096fdd4f8af7529
Star Rolle <starnesharolle13@gmail.com>
2024-01-11 20:51:04 -0500

    is square? method

--- codewars.js
+++ codewars.js
@@ -121,3 +121,7 @@ function boolToWord( bool ){
    })
     return evenOdd % 2 === 0 ? "even" : "odd";
   }
+  //1/11 I answered this first try but may revise it
+  var isSquare = function(n){
+    return Math.sqrt(n)*Math.sqrt(n)===n? true: false
+   }
